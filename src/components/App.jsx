00e4177d39ad0912d8c22a2bf74e640fcbbe613c
PavloPSh <pavlopshe@gmail.com>
2022-10-17 23:14:06 +0300

    add jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,71 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+
+
+import { Section } from "./Section/Section";
+import { Statistics } from "./Statistics/Statistics";
+import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
+import { Notification } from "./Notification/Notification";
+
+
+export class App extends Component{
+  state = {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+  }
+
+  onLeaveFeedback = (propName) => {
+    this.setState((prevState) => {
+      const value = prevState[propName];
+      return {
+        [propName]: value + 1
+      }
+    })
+  }
+  
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return (good + neutral + bad);
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    const result = this.countTotalFeedback() ? (this.state.good / total) * 100 : 0;
+    return Number(result.toFixed(0));
+    }
+
+  render() {
+
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positiveFeedback = this.countPositiveFeedbackPercentage();
+
+    return (
+      <Section title='Please leave feedback'>
+
+        <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={this.onLeaveFeedback} />
+
+        
+        {!total
+          ? <Notification message="There is no feedback" />
+          : <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positiveFeedback={positiveFeedback} />}
+        
+      </Section>
+    )}
+}
+
+
+// export const App = () => {
+//   return (
+//     <div> 
+//       {/* <Feedback /> */}
+//       <Section title='Please leave feedback'>
+//       </Section>
+//     </div> 
+//   );
+// };
